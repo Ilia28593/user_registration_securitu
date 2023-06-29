b350e48ecd9b4f1a178cf9b0b83cf3a1e7f53fa8
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-06-29 23:04:17 +0300

    FlyWay

--- src/main/java/dev/repository/UserRepository.java
+++ src/main/java/dev/repository/UserRepository.java
@@ -2,29 +2,12 @@ package dev.repository;
 
 import dev.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    //    @Query("""
-//            select *
-//            from user
-//            where user_email = :email
-//            """)
     Optional<User> findByEmail(String email);
-
-//    @Transactional
-//    @Modifying
-//    @Query(nativeQuery = true, value = """
-//            insert into person(user_email, first_name, last_name, password, roles)
-//                values
-//                    ('dsdsdsd' ,'31232', '321312', '123213', 'dsdsdd')
-//            """)
-//    void insert();
 }
 
---
+++ src/main/resources/db.migration/V2_Add_admin.sql
@@ -0,0 +1,2 @@
+insert into person(user_email, password, first_name, last_name, birthday, roles)
+values ('admin@gmail.com', '12345A', 'Sidorov', 'Ivan', '2001-05-12', 'ADMIN')
